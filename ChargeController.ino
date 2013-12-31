bd67cc1070f728fdd8211cd8831c84b713d0c9d5
Patrick Friedel <pfriedel@gmail.com>
2013-12-30 23:10:06 -0600

    Better commentary, slightly nicer display routines.

--- ChargeController.ino
+++ ChargeController.ino
@@ -1,21 +1,30 @@
 #include "TinyWireM.h"
 #include <EEPROM.h>
-#include "RealTimeClockDS1307.h"
 
+/* 
+   the only i2c device I have on hand that I can use as a proof of concept is a
+   DS1307.  So I'll toggle the SQW pin. 
+*/
+
+#include "RealTimeClockDS1307.h"
 
 byte __attribute__ ((section (".noinit"))) last_mode;
 
-int MAX_MODE = 5;
+int MAX_MODE = 5; /* how many LEDs do you have? (from zero) */
 
 uint8_t led_grid[6] = {0,0,0,0,0,0};
+
+/* ( AnodePin | CathodePin ) for each LED */
 const uint8_t led_dir[6] = {
-  ( 1<<3 | 1<<4 ), // 1
-  ( 1<<1 | 1<<4 ), // 2
-  ( 1<<4 | 1<<3 ), // 3
-  ( 1<<4 | 1<<1 ), // 4
-  ( 1<<1 | 1<<3 ), // 5
-  ( 1<<3 | 1<<1 ), //6
+  ( 1<<3 | 1<<4 ), /* 1 */
+  ( 1<<1 | 1<<4 ), /* 2 */
+  ( 1<<4 | 1<<3 ), /* 3 */
+  ( 1<<4 | 1<<1 ), /* 4 */
+  ( 1<<1 | 1<<3 ), /* 5 */
+  ( 1<<3 | 1<<1 ), /* 6 */
 };
+
+/* ( AnodePin) */
 const uint8_t led_out[6] = {
   ( 1<<3 ),
   ( 1<<1 ),
@@ -25,52 +34,50 @@ const uint8_t led_out[6] = {
   ( 1<<3 ),
 };
 
-int b1 = 0;
-int b2 = 0;
 uint16_t b;
 uint8_t led, drawcount;
-uint32_t end_time = 0;
 
-
-// invert the logic - update the LEDs during the interrupt, constantly draw them otherwise.
+/* invert the logic - update the LEDs during the interrupt, constantly draw them otherwise. */
 ISR(TIMER0_OVF_vect) { 
 
-  // How many times should the routine loop through the array before returning?
-  // It's a scaling factor - one pass makes the display dimmer, 8 passes makes
-  // the ISR time out.  Arguably this could also be handled by setting the
-  // overflow count to 255>>5 so the ISR gets called 7 times more often than
-  // 62500 times/second. (16,000,000 mhz / 256)
-
+  /* this is a nasty hangover from earlier versions, but I haven't the heart to
+     take it out in case I actually need more repetition. */
   for(drawcount=0; drawcount<2; drawcount++)  {  
+    /* for every LED in the array */
     for(led = 0; led<6; led++) {
+      /* light that LED for the percentage of time from 0 to the LED's brightness */
       for( b=0; b < led_grid[led]; b++ ) {
 	DDRB = led_dir[led];
 	PORTB = led_out[led];
       }
       
-      // and turn the LEDs off for the amount of time in the led_grid array
-      // between LED brightness and 255>>DEPTH.
+      /* and turn the LEDs off for the amount of time in the led_grid array
+	 between LED brightness and 255 */
       for( b=led_grid[led]; b < 255; b++ ) {
 	DDRB = 0;
 	PORTB = 0;
       }
     }
+
+    /* turn everything off at the end in case the last LED was still lit at the end. */
     DDRB=0;
     PORTB=0;
   }
 }
 
-//might be useful since all I'm ever lighting is a single LED..
-void light_led(uint8_t led_num) {
-  DDRB = led_dir[led_num];
-  PORTB = led_out[led_num];
-}
+/* might be useful since all I'm ever lighting is a single LED..
+   void light_led(uint8_t led_num) {
+   DDRB = led_dir[led_num];
+   PORTB = led_out[led_num];
+   }
+   
+   void leds_off() {
+   DDRB = 0;
+   PORTB = 0;
+   }
+*/
 
-void leds_off() {
-  DDRB = 0;
-  PORTB = 0;
-}
-void EEReadSettings (void) {  // TODO: Detect ANY bad values, not just 255.
+void EEReadSettings (void) {
   byte detectBad = 0;
   byte value = 255;
 
@@ -78,17 +85,18 @@ void EEReadSettings (void) {  // TODO: Detect ANY bad values, not just 255.
   if (value > MAX_MODE)
     detectBad = 1;
   else
-    last_mode = value;  // MainBright has maximum possible value of 8.
+    last_mode = value;
 
-  if (detectBad)
-    last_mode = 0; // I prefer the rainbow effect.
+  if (detectBad) /* or merely unitialized */
+    last_mode = 0;
 }
 
 void EESaveSettings (void){
-  //EEPROM.write(Addr, Value);
+  /* EEPROM.write(Addr, Value);
 
-  // Careful if you use  this function: EEPROM has a limited number of write
-  // cycles in its life.  Good for human-operated buttons, bad for automation.
+     Careful if you use this function: EEPROM has a limited number of write
+     cycles in its life.  Good for human-operated buttons, bad for automation.
+  */
 
   byte value = EEPROM.read(0);
 
@@ -97,23 +105,33 @@ void EESaveSettings (void){
 }
 
 void setup() {
-  if(bit_is_set(MCUSR, PORF)) { // Power was just established!
-    MCUSR = 0; // clear MCUSR
-    EEReadSettings(); // read the last mode out of eeprom
+
+  /*
+    Uses the MCUSR flag to determine if it was a cold or a warm startup. A cold
+    startup resumes the prior setting.  A warm startup advances the mode by one
+    and goes to the main execution loop.  If the loop times out, the setting is
+    saved.  Since the EEPROM has more than enough writes, I'm not terribly
+    worried.
+  */
+
+  if(bit_is_set(MCUSR, PORF)) { /* Power was just established! */
+    MCUSR = 0;                  /* clear MCUSR */
+    EEReadSettings();           /* read the last mode out of eeprom */
   }
-  else if(bit_is_set(MCUSR, EXTRF)) { // we're coming out of a reset condition.
-    MCUSR = 0; // clear MCUSR
-    last_mode++; // advance mode
+  else if(bit_is_set(MCUSR, EXTRF)) { /* we're coming out of a reset condition. */
+    MCUSR = 0;                        /* clear MCUSR */
+    last_mode++;                      /* advance mode */
     
     if(last_mode > MAX_MODE) {
-      last_mode = 0; // reset mode
+      last_mode = 0;                  /* reset mode */
     }
   }
 
+  /* Set the timer overflow flags */
   TCCR0B |= (1<<CS00);
   TIMSK |= 1<<TOIE0;
 
-  // may not actually be necessary, but it seems to help get the clock started...
+  /* may not actually be necessary, but it seems to help get the clock started... */
   TinyWireM.begin();
   RTC.switchTo24h();
   RTC.stop();
@@ -127,23 +145,26 @@ void setup() {
   RTC.setClock();
   RTC.start();
   RTC.readClock();
+
+  /* start servicing interrupts. */
   sei();
 }
 
 void loop() {
-  // indicate which mode we're entering
+  /* indicate which mode we're entering */
   for(int x = 0; x <= 255; x++) {
     led_grid[last_mode] = x;
-    delay(250/int(255));
+    delay(1);
   }
   delay(500);
   for(int x = 255; x>=0; x--) {
     led_grid[last_mode] = x;
-    delay(250/int(255));
+    delay(1);
   }
 
   delay(100);
-  
+
+  /* if the user hasn't reset by now, assume they want the current mode.  Save it. */
   EESaveSettings();
 
   bool sqw_out = true;
@@ -151,15 +172,15 @@ void loop() {
   while(1) {
     for(int x = 0; x<=5; x++) {
       led_grid[x] = 255;
-      delay(250);
+      delay(500);
       led_grid[x] = 0;
 
     
       /* 
-	 Merely clearing interrupts isn't sufficient since I'm jerking around the
-	 state of the port B registers in the interrupt.  So initialize the USI interface
-	 again and bob's your uncle.  Only works (so far) at 8mhz, but I only need 8mhz
-	 to keep 6 LEDs lit without flickering them.
+	 Merely clearing interrupts isn't sufficient since I'm jerking around
+	 the state of the port B registers in the interrupt.  So initialize the
+	 USI interface again and bob's your uncle.  Only works (so far) at 8mhz,
+	 but I only need 8mhz to keep 6 LEDs lit without flickering them.
       */
       cli();
       TinyWireM.begin();
@@ -173,9 +194,6 @@ void loop() {
       }
       sei();
     }
-
-    
-
   }
 }
 
